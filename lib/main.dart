0ca2b7a9b0f779ad9cc62b29cadc84be323f157f
Mahir Abd Alazim <mahr.bwb@gmail.com>
2021-05-27 14:20:11 -0700

    Create Project Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,89 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  final List<Transaction> transaction = [
+    Transaction(
+        id: 't1', title: 'New Shirt', date: DateTime.now(), amount: 99.4),
+    Transaction(
+        id: 't2', title: 'New Phone', date: DateTime.now(), amount: 199.4),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        children: [
-          Card(
-            child: Text('CHART !'),
-          ),
-          Card(
-            child: Text('LIST OF TX'),
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personal Expenses'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.purple,
+                child: Text('CHART !'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.purple),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.green.shade100,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                             style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 10,
+                               color: Colors.grey
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/transaction.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/foundation.dart';
+class Transaction {
+  String id;
+
+  String title;
+  double amount;
+
+  DateTime date;
+
+  Transaction({@required this.id, @required this.title,  @required this.date,@required this.amount});
+}
