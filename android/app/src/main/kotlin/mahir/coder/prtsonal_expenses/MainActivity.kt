db2364824c00c0e817a61e5400f40d88fdcf69bb
Mahir Abd Alazim <mahr.bwb@gmail.com>
2021-05-27 12:16:02 -0700

    Create Project Setup

---
+++ android/app/src/main/kotlin/mahir/coder/prtsonal_expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package mahir.coder.prtsonal_expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Text('CHART !'),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
